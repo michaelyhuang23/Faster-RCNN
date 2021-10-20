f17c411d24288715fb660d3dce4890f137fed42c
Michael Huang <michaelyhuang23@gmail.com>
2021-10-20 00:30:30 -0400

    update trainer again

--- trainer.py
+++ trainer.py
@@ -7,21 +7,28 @@ from torch.utils.data import DataLoader
 import utils
 from engine import train_one_epoch, evaluate
 from torch.utils.tensorboard import SummaryWriter
+import pickle
 
 model = fasterrcnn_resnet50_fpn(pretrained=False, pretrained_backbone=True)
 
 class DetectDataset(CocoDetection):
-    def __init__(self, root, annFile) -> None:
+    def __init__(self, root, annFile, indices_file=None) -> None:
         super().__init__(root, annFile, transforms.ToTensor())
         self.novel_cls = [1, 17, 16, 21, 18, 19, 20, 2, 9, 6, 3, 4, 7, 44, 62, 67, 64, 63, 72]
         self.base_cls = [i for i in range(1,91) if i not in self.novel_cls]
-        self.valid_index = list(range(super().__len__()))
-        new_valid_index = []
-        for i in range(super().__len__()):
-            img, anno = self[i]
-            if img != None and anno != None:
-                new_valid_index.append(i)
-        self.valid_index = new_valid_index
+        if indices_file is None:
+            self.valid_index = list(range(super().__len__()))
+            new_valid_index = []
+            for i in range(super().__len__()):
+                img, anno = self[i]
+                if img != None and anno != None:
+                    new_valid_index.append(i)
+            self.valid_index = new_valid_index
+            with open('valid_indices_coco', 'wb') as f:
+                pickle.dump(self.valid_index, f)
+        else:
+            with open(indices_file,'rb') as f:
+                self.valid_index = pickle.load(f)
 
     def __getitem__(self, index):
         index = self.valid_index[index]
