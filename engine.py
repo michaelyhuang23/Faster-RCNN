2f415addbceea70dd080abb5ef9ad8e6db6e27ea
Michael Huang <michaelyhuang23@gmail.com>
2021-10-20 00:42:15 -0400

    optim train

--- engine.py
+++ engine.py
@@ -21,22 +21,18 @@ def train_one_epoch(writer, model, optimizer, data_loader, device, epoch, print_
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         loss_dict = model(images, targets)
         losses = sum(loss for loss in loss_dict.values())
-        # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = utils.reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
-        loss_value = losses_reduced.item()
-
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            print(loss_dict_reduced)
+        loss_val = losses.cpu().detach().item()
+        if not math.isfinite(loss_val):
+            print("Loss is {}, stopping training".format(loss_val))
+            print(loss_val)
             sys.exit(1)
 
         optimizer.zero_grad()
         losses.backward()
         optimizer.step()
-        writer.add_scalar('Loss/train', losses.cpu().item(), epoch*len(data_loader)+i)
-        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        writer.add_scalar('Loss/train', losses.cpu().detach().item(), epoch*len(data_loader)+i)
+        metric_logger.update(loss=loss_val, **loss_dict.detach())
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
     return metric_logger
